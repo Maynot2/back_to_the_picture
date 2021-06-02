3c79858475b7556684877220d9c9599424d63258
polsud <paulmanot@gmail.com>
2021-06-02 10:12:13 +0200

    Fix color schemme and work on navbar

--- react_bttp/src/components/NavBar.js
+++ react_bttp/src/components/NavBar.js
@@ -1,24 +1,27 @@
-import {ReactComponent as ReactLogo} from './BTTP-logo-full-white.svg';
+import {ReactComponent as ReactLogoFull} from './BTTP-logo-full-white.svg';
+import {ReactComponent as ReactLogoSmall} from './BTTP-logo-sm-white.svg';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <div className="bg-nav-gradient border-b-4  border-secondary">
-      <div className="max-w-7xl mx-auto p-4 flex justify-between items-center">
-        <div className="">
-          <ReactLogo className="w-52"/>
-        </div> 
-        <button className="bg-secondary border-2 border-neutralW text-neutralW hover:bg-neutralW hover:text-secondary hover:border-secondary font-bold py-4 px-10 rounded-full">
+      <nav className="xl:max-w-7xl mx-auto p-4 flex justify-between items-center">
+        <Link to='/' className="pr-4">
+          <ReactLogoFull className="hidden sm:block w-52"/>
+          <ReactLogoSmall className="sm:hidden w-36"/>
+        </Link> 
+        <Link to='#' className="hidden sm:block bg-secondary border-2 border-neutralW text-neutralW hover:bg-neutralW hover:text-secondary hover:border-secondary font-bold py-4 px-10 rounded-full transition duration-300">
           upload pic
-        </button>
-        <div className="flex">
-          <button className="bg-transparent text-tertiary border-tertiary border-2 hover:bg-tertiary hover:text-primary font-semibold py-2 px-4 rounded">
-            sign-up
-          </button>
-          <button className="bg-transparent ml-2 text-tertiary border-tertiary border-2 hover:bg-tertiary hover:text-primary font-semibold py-2 px-4 rounded">
+        </Link>
+        <div className="hidden sm:flex ">
+          <Link to='#' className="bg-transparent text-tertiary hover:text-tertiary-dark font-semibold hover:font-black py-2 px-4">
             log-in
-          </button>
+          </Link>
+          <Link to='#' className="bg-transparent text-tertiary border-tertiary border-2 hover:bg-tertiary hover:text-primary font-semibold ml-2 py-2 px-4 rounded">
+            sign-up
+          </Link>
         </div>
-      </div>
+      </nav>
     </div>
   );
 }
--- react_bttp/tailwind.config.js
+++ react_bttp/tailwind.config.js
@@ -2,13 +2,15 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: 'var(--color-primary)',
-      secondary: 'var(--color-secondary)',
-      tertiary: 'var(--color-tertiary)',
-      neutralW:'var(--color-neutral-w)',
-      neutralB:'var(--color-neutral-b)',
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        tertiary: 'var(--color-tertiary)',
+        'tertiary-dark': 'var(--color-tertiary-dark)',
+        neutralW:'var(--color-neutral-w)',
+        neutralB:'var(--color-neutral-b)',
+      }
     }
   },
   variants: {
