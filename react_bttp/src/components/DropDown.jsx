eb58eb7305ea63e1c8b47190001d394bdae9e435
polsud <paulmanot@gmail.com>
2021-06-07 15:41:16 +0200

    Finish navbar design

---
+++ react_bttp/src/components/DropDown.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const DropDown = ({ isOpen, toggleMenu }) => {
+  return (
+    <div className="absolute w-full">
+      <nav
+        className={
+          isOpen
+            ? "flex flex-col text-neutralW text-center bg-primary sm:hidden"
+            : "hidden"
+        }
+        onClick={toggleMenu}
+      >
+        <Link to="" className="border-b-2 border-neutralW py-3">
+          log-in
+        </Link>
+        <Link to="" className="border-b-2 border-neutralW py-3">
+          sign-up
+        </Link>
+        <Link to="" className="border-b-2 border-neutralW py-3">
+          upload pic
+        </Link>
+      </nav>
+    </div>
+  );
+};
