3603d1412a33e8fd547bf319781b165b4d20bf46
vagrant <vagrant@vagrant.vm>
2021-06-11 07:31:53 +0000

    fix album routes

--- back/api/routes/albumRoutes.js
+++ back/api/routes/albumRoutes.js
@@ -25,7 +25,7 @@ module.exports = function(router) {
         if (!album) {
           throw Error;
         }
-        res.json(spot);
+        res.json(album);
     } catch (error) {
       res.status(404).send(
         {
@@ -52,22 +52,21 @@ module.exports = function(router) {
     }
   });
 
-  // doesn't work "column \"userId\" does not exist"
   router.post("/api/albums/create", async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw Error;
     }
-    const { userId, name, takenAt, spotId } = req.body; 
+    const { name, userId, spotId, takenAt } = req.body; 
     console.log(req.body);
     try {
       const createdAlbum = await Album.create({
-        userId,
         name,
-        takenAt,
-        spotId
+        userId,
+        spotId,
+        takenAt
       });
-      res.status(201).json({spot: createdAlbum});
+      res.status(201).json({album: createdAlbum});
     } catch (error) {
       res.status(422).send(
         {
@@ -76,31 +75,19 @@ module.exports = function(router) {
       }
     });
 
-  // router.post("/api/albums/create", (req, res) => {
-  //   Album.create({
-  //     name: req.body.name,
-  //     userId: req.body.userId,
-  //     spotId: req.body.spotId,
-  //     takenAt: req.body.takenAt
-  //   })
-  //     .then(res => {
-  //       res.json(res);
-  //     })
-  //     .catch(err => res.json(err));
-  // });
 
   router.put('/api/albums/:id', async (req, res) => {
-    const { userId, name, takenAt, spotId } = req.body; 
+    const { name, userId, spotId, takenAt } = req.body; 
     try {
       const updatedAlbum = await Album.findByPk(req.params.id);
       if (!updatedAlbum) {
         res.send('Album not found');
       } else {
         await updatedAlbum.update({
-          userId,
           name,
-          takenAt,
-          spotId
+          userId,
+          spotId,
+          takenAt
         });
         res.status(201).json({place: updatedAlbum});
       }
@@ -112,21 +99,7 @@ module.exports = function(router) {
     }
   });
 
-  // router.put("/api/albums/:id", (req, res) => {
-  //   Album.update({
-  //       name: req.body.name,
-  //       userId: req.body.userId,
-  //       spotId: req.body.spotId,
-  //       takenAt: req.body.takenAt },
-  //       { where: { id: req.params.id } }
-  //   )
-  //     .then(updatedAlbum => {
-  //       res.json(updatedAlbum);
-  //     })
-  //     .catch(err => res.json(err));
-  // });
-
-  router.delete("/albums/:id", (req, res) => {
+  router.delete("/api/albums/:id", (req, res) => {
     Album.destroy({
       where: { id: req.params.id }
     })
