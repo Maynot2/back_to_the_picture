ee500e9ab5ccb53e65e40dfafa37db23866e34a1
polsud <paulmanot@gmail.com>
2021-06-22 17:58:10 +0200

    Remove useless if

--- react_bttp/src/index.js
+++ react_bttp/src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 import Albums from "./components/Albums/Index";
 
-const redirectUrl =`${if process.env.NODE_ENV === "production" ? "https://backtothepicture.me" : "http://localhost:3000"}`
+const redirectUrl =`${process.env.NODE_ENV === "production" ? "https://backtothepicture.me" : "http://localhost:3000"}`
 
 ReactDOM.render(
   <React.StrictMode>
