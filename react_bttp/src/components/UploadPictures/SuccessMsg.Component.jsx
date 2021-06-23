d7a9bdb4d4348ff3660828bea1a2ad9b6cf97eb8
polsud <paulmanot@gmail.com>
2021-06-23 11:31:06 +0200

    Rm link to uploaded img

--- react_bttp/src/components/UploadPictures/SuccessMsg.Component.jsx
+++ react_bttp/src/components/UploadPictures/SuccessMsg.Component.jsx
@@ -4,7 +4,6 @@ function SuccessMsg({ url }) {
   return (
     <div>
       <h3 style={{ color: "green" }}>SUCCESSFUL UPLOAD</h3>
-      <a href={url[0]}>Access the file here</a>
       <br />
     </div>
   );
